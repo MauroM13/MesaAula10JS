ca0a3a4ef28e6fd265521044fc2feb2f616c7623
MauroM13 <mauroluciomartins@gmail.com>
2021-08-31 19:35:53 -0300

    tentativa de arrays

---
+++ Arrays.js
@@ -0,0 +1,11 @@
+
+var arr = [1, 2, 3, 4, 5];
+arr.reverse();
+console.log(arr); 
+
+let arr = [1, 2, 3, 4, 5, ];
+let total = numeros.reduce(function(total, numero){
+return total + numero;
+}, 0);
+console.log(total);
+
